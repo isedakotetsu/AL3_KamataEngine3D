85e36337f5ae2c91280e152aa3f60bab5a7f82f0
isedakotetsu <k024g0050@g.neec.ac.jp>
2025-04-11 12:28:03 +0900

    kamataいれた

---
+++ DirectXGame/GameScene.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "KamataEngine.h"
+
+
+class GameScene 
+{
+public:
+
+	void Initialize();
+
+	void Update();
+
+	void Draw();
+
+};
+
---
+++ DirectXGame/main.cpp
@@ -0,0 +1,44 @@
+#include <Windows.h>
+#include "kamataEngine.h"
+#include "GameScene.h"
+
+using namespace KamataEngine;
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) 
+{
+	GameScene* gameScene = new GameScene();
+
+	gameScene->Initialize();
+
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+	KamataEngine::Initialize(L"LE2C_04_イセダ_コテツ_AL3");
+
+
+	while (true) 
+	{
+
+		if (KamataEngine::Update())
+		{
+			break;
+		}
+		//ゲームシーンの更新
+		gameScene->Update();
+
+		//描画開始
+		dxCommon->PreDraw();
+
+		//ゲームシーンの描画
+		gameScene->Draw();
+
+		//描画終了
+		dxCommon->PostDraw();
+	}
+
+	KamataEngine::Finalize();
+	delete gameScene;
+
+	gameScene = nullptr;
+	
+	return 0;
+}
