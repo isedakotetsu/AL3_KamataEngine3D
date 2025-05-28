8ae667dc2cea713dfb8756aa2a2aeef751173914
isedakotetsu <k024g0050@g.neec.ac.jp>
2025-05-28 11:52:20 +0900

    自キャラの基本挙動

--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -45,9 +45,9 @@ private:
 
 	Vector3 velocity_ = {};
 
-	static inline const float kAcceleration = 0.05f;
-	static inline const float kAttenuation = 0.05f;
-	static inline const float kLimitRunSpeed = 0.05f;
+	static inline const float kAcceleration = 0.1f;
+	static inline const float kAttenuation = 0.1f;
+	static inline const float kLimitRunSpeed = 0.1f;
 
 	LRDirection lrDirection_ = LRDirection::kRight;
 
