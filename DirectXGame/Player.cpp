a0f874e1b7803ef9b429fd367e4ef8d7d93cd59f
isedakotetsu <k024g0050@g.neec.ac.jp>
2025-10-15 14:52:40 +0900

    hozon

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -438,33 +438,73 @@ void Player::BehaviorRootUpdate()
 }
 void Player::BehaviorAttackUpdate() 
 {
-	// 移動入力(02_07 スライド10枚目)
-	InputMove();
+	const Vector3 attackVelocity = {0.8f, 0.0f, 0.0f};
 
-	// 衝突情報を初期化(02_07 スライド13枚目)
+	Vector3 velocity{};
+
+	attackParameter_++;
+
+	switch (attackPhase_) {
+	case AttackPhase::kAnticipation: // 溜め動作
+	// 02_14 26枚目
+	default: {
+		velocity = {};
+		float t = static_cast<float>(attackParameter_) / kAnticipationTime;
+		worldTransform_.scale_.z = EaseOut(1.0f, 0.3f, t);
+		worldTransform_.scale_.y = EaseOut(1.0f, 1.6f, t);
+
+		// 前進動作へ移行
+		if (attackParameter_ >= kAnticipationTime) {
+			attackPhase_ = AttackPhase::kAction;
+			attackParameter_ = 0; // カウンターをリセット
+		}
+		break;
+	}
+
+	case AttackPhase::kAction: { // 突進動作
+		if (lrDirection_ == LRDirection::kRight) {
+			velocity += attackVelocity;
+		} else {
+			velocity -= attackVelocity;
+		}
+
+		float t = static_cast<float>(attackParameter_) / kActionTime;
+		worldTransform_.scale_.z = EaseOut(0.3f, 1.3f, t);
+		worldTransform_.scale_.y = EaseIn(1.6f, 0.7f, t);
+
+		// 余韻動作へ移行
+		if (attackParameter_ >= kActionTime) {
+			attackPhase_ = AttackPhase::kRecovery;
+			attackParameter_ = 0; // パラメータをリセット
+		}
+	} break;
+
+	case AttackPhase::kRecovery: { // 余韻動作
+		velocity = {};
+		float t = static_cast<float>(attackParameter_) / kRecoveryTime;
+		worldTransform_.scale_.z = EaseOut(1.3f, 1.0f, t);
+		worldTransform_.scale_.y = EaseOut(0.7f, 1.0f, t);
+
+		// 通常行動に戻る
+		if (attackParameter_ >= kRecoveryTime) {
+			behaviorRequest_ = Behavior::kRoot;
+		}
+		break;
+	}
+	}
+
+	// 衝突情報を初期化
 	CollisionMapInfo collisionMapInfo = {};
-	collisionMapInfo.move = velocity_;
+	collisionMapInfo.move = velocity;
 	collisionMapInfo.landing = false;
 	collisionMapInfo.hitWall = false;
 
-	// マップ衝突チェック(02_07 スライド13枚目)
+	// マップ衝突チェック
 	CheckMapCollision(collisionMapInfo);
 
-	// 移動(02_07 スライド36枚目)
+	// 移動
 	worldTransform_.translation_ += collisionMapInfo.move;
 
-	// 天井接触による落下開始(02_07 スライド38枚目)
-	if (collisionMapInfo.ceiling) {
-		velocity_.y = 0;
-	}
-
-	// 02_08 スライド27枚目 壁接触している場合の処理
-	UpdateOnWall(collisionMapInfo);
-
-	// 接地判定
-	UpdateOnGround(collisionMapInfo);
-
-	// 旋回制御
 	if (turnTimer_ > 0.0f) {
 		// タイマーを進める
 		turnTimer_ = std::max(turnTimer_ - (1.0f / 60.0f), 0.0f);
@@ -474,13 +514,6 @@ void Player::BehaviorAttackUpdate()
 		float destinationRotationY = destinationRotationYTable[static_cast<uint32_t>(lrDirection_)];
 
 		worldTransform_.rotation_.y = EaseInOut(destinationRotationY, turnFirstRotationY_, turnTimer_ / kTimeTurn);
-	}
-
-	attackParameter_++;
-
-	if (attackParameter_ >= kAttackTime)
-	{
-		behaviorRequest_ = Behavior::kRoot;
 	}
 }
 
